077bdaf4c1077e84ab92fd3798eb3068c5ffc561
natsuokawai <kawai.natsuo@gmail.com>
2021-09-07 22:44:20 +0900

    Discord time signal request

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/natsuokawai/go-discord-time-signal
+
+go 1.17
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/jkershaw2000/go-tenor v0.0.0-20200728191921-3f3fd5c10bc8
+)
+
+require (
+	github.com/gorilla/websocket v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+)
+
+replace github.com/jkershaw2000/go-tenor => github.com/natsuokawai/go-tenor v0.0.0-20210907130820-38365952e24b
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	gotenor "github.com/natsuokawai/go-tenor"
+)
+
+func main() {
+	url := "https://discord.com/api/webhooks/XXXXXXXXXXXXXXXxx"
+	t := gotenor.NewTenor("YYYYYYYYY")
+	gifURL := getGifURL(t)
+
+	params := &discordgo.WebhookParams{
+		Username: "時報Bot", Content: timeSignalMessage(),
+	}
+	params.Embeds = []*discordgo.MessageEmbed{
+		{Image: &discordgo.MessageEmbedImage{URL: gifURL}},
+	}
+	sendWebhook(url, params)
+}
+
+func timeSignalMessage() string {
+	hour := time.Now().Round(time.Hour).Hour()
+	return strconv.Itoa(hour) + "時です"
+}
+
+func getGifURL(t gotenor.Tenor) string {
+	data, err := t.GetRandom("cat", "", "", 3)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return gotenor.GetAllGifURLs(*data)[2]
+}
+
+// discord request
+func sendWebhook(url string, params *discordgo.WebhookParams) {
+	j, err := json.Marshal(params)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(j))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	if resp.StatusCode == 204 {
+		fmt.Println("successfully sent message")
+		return
+	}
+
+	fmt.Fprintln(os.Stderr, resp)
+}
